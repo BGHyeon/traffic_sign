7fab43fecf2cb9d04742b430d000283341aa147f
BaekGyuHyeon <>
2023-08-17 20:00:08 +0900

    tmp commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import cv2
 from flask import Flask,render_template,request,jsonify
 import base64
+import ex1
 app = Flask(__name__)
-
+model = cv2.dnn.readNet("model/yolov3-tiny.cfg","model/yolov3-tiny.weights")
 @app.route('/')
 def home () :
     return render_template('index.html')
@@ -9,8 +11,10 @@ def home () :
 @app.route('/upload',methods=['POST'])
 def uploadImage () : 
     image = request.files['image']
-    encodeImage = base64.b64encode(image.read()).decode('utf-8')
+    detectedImage,detectedLabel = ex1.detection(image,model)
 
-    return jsonify({'resultImage':encodeImage,'message':'success'})
+    retval, buffer = cv2.imencode('.jpg', detectedImage)
+    encodeImage = base64.b64encode(buffer).decode('utf-8')
+    return jsonify({'resultImage':encodeImage,'message':str(detectedLabel)})
 if __name__ == '__main__':
     app.run(port=8080,debug=True)
