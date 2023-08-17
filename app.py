8bc5015d371e825873f76d370a14d19b2787fec8
BaekGyuHyeon <>
2023-08-17 20:44:47 +0900

    tmp commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask,render_template,request,jsonify
 import detect
-from PIL import Image
-
+import cv2
 import base64
 from io import BytesIO
 app = Flask(__name__)
@@ -14,15 +13,8 @@ def uploadImage () :
     image = request.files['image']
     image.save('tmp.jpg')
     proceedImage = detect.detect(source='./tmp.jpg',weights='./best.pt')
-    rawBytes = BytesIO()
-
-    # 2차원 배열의 type은 uint8이여야 인코딩 가능
-    img_buffer = Image.fromarray(proceedImage[0].astype('uint8'))
-
-    img_buffer.save(rawBytes, 'PNG')
-    rawBytes.seek(0)
-    base64_img = base64.b64encode(rawBytes.read()).decode('utf-8')
-    # encodeImage = base64.b64encode(Image.fromarray(proceedImage[0]).read()).decode('utf-8')
-    return jsonify({'resultImage':base64_img,'message':'success'})
+    retval, buffer = cv2.imencode('.jpg', proceedImage[0][0])
+    encodeImage = base64.b64encode(buffer).decode('utf-8')
+    return jsonify({'resultImage':encodeImage,'message':str(proceedImage[0][1])})
 if __name__ == '__main__':
     app.run(port=8080,debug=True)
--- detect.py
+++ detect.py
@@ -138,6 +138,7 @@ def detect(
         # Process predictions
         for i, det in enumerate(pred):  # per image
             seen += 1
+            detected_labels = []
             if webcam:  # batch_size >= 1
                 p, im0, frame = path[i], im0s[i].copy(), dataset.count
                 s += f'{i}: '
@@ -145,8 +146,8 @@ def detect(
                 p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
 
             p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # im.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
+            # save_path = str(save_dir / p.name)  # im.jpg
+            # txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
             s += '%gx%g ' % im.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
@@ -161,30 +162,23 @@ def detect(
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        with open(f'{txt_path}.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                # for *xyxy, conf, cls in reversed(det):
+                    # if save_txt:  # Write to file
+                    #     xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    #     line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                    #     with open(f'{txt_path}.txt', 'a') as f:
+                    #         f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
                         label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
                         annotator.box_label(xyxy, label, color=colors(c, True))
-                    if save_crop:
-                        save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+                        detected_labels.append(label)
+                    # if save_crop:
+                    #     save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
-            # Stream results
-            im0 = annotator.result()
-            if view_img:
-                if platform.system() == 'Linux' and p not in windows:
-                    windows.append(p)
-                    cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
-                    cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-            ret_list.append(im0)
+            # Stream result
+            ret_list.append((im0,detected_labels))
             # Save results (image with detections)
         return ret_list
             #
